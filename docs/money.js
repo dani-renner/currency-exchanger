96dd6339da02a227110932c2d2884bdfdd77b88e
Dani Renner <danijrenner@gmail.com>
2021-02-12 15:22:04 -0800

    add error handling

--- docs/money.js
+++ docs/money.js
@@ -1,13 +1,14 @@
 export default class MoneyService {
-  static async getRate(countryCode){
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/GBP/${countryCode}`);
-      if(!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    } catch(error) {
-      return error.message;
+  static getRate(countryCode){
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/GBP/${countryCode}`)
+      .then(function(response){
+        if(!response.ok){
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error){
+        return error;
+      });
     }
-  }
 }
