6d9eb174b792e378faed0700665257f46c73eeee
Dani Renner <danijrenner@gmail.com>
2021-02-12 16:38:07 -0800

    try with static async

--- docs/money.js
+++ docs/money.js
@@ -1,14 +1,13 @@
 export default class MoneyService {
-  static getRate(countryCode){
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/GBP/${countryCode}`)
-      .then(function(response){
-        if(!response.ok){
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error){
-        return error;
-      });
+  static async getRate(countryCode){
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/enriched/GBP/${countryCode}`);
+      if(!response.ok){
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error){
+      return error.message;
     }
+  }
 }
