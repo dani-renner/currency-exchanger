26b29945a46eedcfaceb75477faf2c5fad7b77cb
Dani Renner <danijrenner@gmail.com>
2021-02-18 17:48:54 -0800

    add api key back in since error works

--- src/js/money.js
+++ src/js/money.js
@@ -1,7 +1,7 @@
 export default class MoneyService {
   static async getRate(){
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/86/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if(!response.ok){
         throw Error(response.statusText);
       }
@@ -11,4 +11,3 @@ export default class MoneyService {
     }
   }
 }
-//${process.env.API_KEY}
